65b940b2c6ab6d424c184ff039d0ba01917c3bdb
Siddharth77s <siddharthshinde9476@gmail.com>
2025-07-21 12:35:27 +0530

    Time: 2 ms (80.39%) | Memory: 46.5 MB (18.04%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,22 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int i = 0;
+        int j = 0;
+        // 0 to j-1 => all nonzeroes
+        //j to i-1 => all zeroes
+        // i to end => unknown
+
+        // used quicksort algorithm
+        while(i < nums.length){
+            if(nums[i]==0){
+                i++;
+            }else{
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                i++;
+                j++;
+            }
+        }
+    }
+}
